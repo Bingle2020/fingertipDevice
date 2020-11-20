e1e46998fe48eb06911eeac0d36d08019c1ee3b7
Bingle2020 <13670042232@163.com>
2020-11-20 16:51:28 +0800

    install less

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="home">
         <van-nav-bar title="主页" left-arrow/>
+        <h1 class="px">px大小测试</h1>
     </div>
 </template>
 
@@ -8,6 +9,16 @@
 export default {}
 </script>
 
-<style lang="scss" scoped>
+<style lang="less" scoped>
+.home {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
 
+    .px {
+        font-size: 25px;
+    }
+}
 </style>
